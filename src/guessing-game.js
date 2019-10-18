872eebd402741e98d272ae7a75fa833a035279dc
kalachou <mvkalach>
2019-10-18 05:36:55 +0300

    guessing-game(feat): done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.currGuess = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.currGuess = Math.round(this.min + (this.max - this.min)/2);
+    	return this.currGuess;
     }
 
     lower() {
-
+    	this.max = this.currGuess;
     }
 
     greater() {
-
+    	this.min = this.currGuess;
     }
 }
 
